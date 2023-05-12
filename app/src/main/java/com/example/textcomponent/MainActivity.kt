19284a0c2f968579ff81919477fc79638c9a824f
mubaraqadenle <adenlemubaraq@gmail.com>
2023-05-12 18:56:21 +0100

    Text with string plural resource

--- app/src/main/java/com/example/textcomponent/MainActivity.kt
+++ app/src/main/java/com/example/textcomponent/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.textcomponent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.PluralsRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -57,6 +62,24 @@ fun textWithTextStyle(){
 @Composable
 fun DefaultPreview() {
     TextComponentTheme {
-        Text(text = "My Sample Large ", style = MaterialTheme.typography.headlineLarge)
+        TextWithQuantity()
     }
 }
+
+@Composable
+fun quantityStringResource(@PluralsRes pluralResId: Int, quantity: Int, vararg formatArgs: Any? = emptyArray()): String
+{
+    return LocalContext.current.resources.getQuantityString(pluralResId, quantity, *formatArgs)
+}
+
+@Composable
+fun TextWithQuantity(){
+    Column {
+        Text(quantityStringResource(pluralResId = R.plurals.contact, quantity = 0,0))
+        Text(quantityStringResource(pluralResId = R.plurals.contact, quantity = 1,1))
+        Text(quantityStringResource(pluralResId = R.plurals.contact, quantity = 2,2))
+        Text(quantityStringResource(pluralResId = R.plurals.contact, quantity = 3,3))
+        Text(quantityStringResource(pluralResId = R.plurals.contact, quantity = 10,10))
+    }
+}
+
